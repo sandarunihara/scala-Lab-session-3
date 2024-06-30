cc244fd22df5f25866b325879602a1d9634a41a9
sandarunihara <sandarunihara56@gmail.com>
2024-06-30 14:55:26 +0530

    3

--- lab3.scala
+++ lab3.scala
@@ -9,3 +9,11 @@ def filterStrings(strings: List[String]): List[String] = {
     strings.filter(_.length > 5)
 }
  val input = List("Scala", "programming", "is", "fun", "and", "productive")
+
+
+
+ def calculateMean(num1: Double, num2: Int): Double = {
+    val mean = (num1 + num2) / 2.0
+    // Round to two decimal places using basic arithmetic
+    Math.round(num1 * 100.0) / 100.0
+  }
